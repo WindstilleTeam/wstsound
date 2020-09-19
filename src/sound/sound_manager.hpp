ce27fb405425b2b9a89dd6bdb6e6ba2c429e605f
Ingo Ruhnke <grumbel@gmail.com>
2020-09-19 14:45:24 +0200

    Remove music handling from SoundManager

--- src/sound/sound_manager.hpp
+++ src/sound/sound_manager.hpp
@@ -42,7 +42,6 @@ public:
   ~SoundManager();
 
   void enable_sound(bool sound_enabled);
-  void enable_music(bool music_enabled);
 
   void set_listener_position(const glm::vec2& position);
   void set_listener_velocity(const glm::vec2& velocity);
@@ -58,9 +57,6 @@ public:
    * Convenience function to simply play a sound at a given position.
    */
   SoundSourcePtr play(std::filesystem::path const& filename, const glm::vec2& pos = glm::vec2(-1, -1));
-
-  void play_music(std::filesystem::path const& filename, bool fade = true);
-  void stop_music(bool fade = true);
 
   void update(float delta);
 
