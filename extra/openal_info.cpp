5e59d0341ec8daaa566a9e4fafa22ba0e1083596
Ingo Ruhnke <grumbel@gmail.com>
2020-09-20 23:10:28 +0200

    Add extra/openal_info

---
+++ extra/openal_info.cpp
@@ -0,0 +1,30 @@
+/*
+**  Windstille - A Sci-Fi Action-Adventure Game
+**  Copyright (C) 2018 Ingo Ruhnke <grumbel@gmail.com>
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+
+#include "sound/openal_system.hpp"
+
+int main(int argc, char** argv)
+{
+  OpenALSystem openal;
+  openal.print_openal_version(std::cout);
+  return 0;
+}
+
+/* EOF */
