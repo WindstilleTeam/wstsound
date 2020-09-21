a51265774605a3fdc442f68f9e759042718d10ce
Ingo Ruhnke <grumbel@gmail.com>
2020-09-21 14:00:28 +0200

    Add experimental ProceduralSoundFile

---
+++ src/sound/procedural_sound_file.cpp
@@ -0,0 +1,80 @@
+/*
+**  Windstille - A Sci-Fi Action-Adventure Game
+**  Copyright (C) 2018 Ingo Ruhnke <grumbel@gmail.com>
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "sound/procedural_sound_file.hpp"
+
+#include <iostream>
+#include <limits>
+#include <math.h>
+
+namespace {
+
+template<typename T>
+T map_to(float value)
+{
+  if (value >= 0) {
+    return static_cast<T>(static_cast<float>(std::numeric_limits<T>::max()) * value);
+  } else {
+    return static_cast<T>(-static_cast<float>(std::numeric_limits<T>::min()) * value);
+  }
+}
+
+} // namespace
+
+ProceduralSoundFile::ProceduralSoundFile() :
+  m_bits_per_sample(16),
+  m_rate(48000),
+  m_channels(1),
+  m_size(100000000000),
+  m_sample_pos(0)
+{
+}
+
+size_t
+ProceduralSoundFile::read(void* buffer, size_t buffer_size)
+{
+  std::cout << "read():" << buffer_size << std::endl;
+  int16_t* samples = static_cast<int16_t*>(buffer);
+  size_t len = buffer_size / sizeof(int16_t);
+
+  for(size_t i = 0; i < len; ++i) {
+    size_t const pos = m_sample_pos + i;
+    float const pos_sec = static_cast<float>(pos) / static_cast<float>(m_rate);
+    float const value = sinf(pos_sec * 5000.0f);
+    samples[i] = map_to<int16_t>(value);
+    //samples[i] = static_cast<int16_t>(rand() % 30000) / 4 + map_to<int16_t>(value);
+  }
+
+  m_sample_pos += len;
+
+  return buffer_size;
+}
+
+void
+ProceduralSoundFile::reset()
+{
+  m_sample_pos = 0;
+}
+
+void
+ProceduralSoundFile::seek_to_sample(int sample)
+{
+  m_sample_pos = sample;
+}
+
+/* EOF */
---
+++ src/sound/procedural_sound_file.hpp
@@ -0,0 +1,54 @@
+/*
+**  Windstille - A Sci-Fi Action-Adventure Game
+**  Copyright (C) 2018 Ingo Ruhnke <grumbel@gmail.com>
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef HEADER_WINDSTILLE_SOUND_PROCEDURAL_SOUND_FILE_HPP
+#define HEADER_WINDSTILLE_SOUND_PROCEDURAL_SOUND_FILE_HPP
+
+#include "sound/sound_file.hpp"
+
+class ProceduralSoundFile : public SoundFile
+{
+private:
+public:
+  ProceduralSoundFile();
+
+  size_t read(void* buffer, size_t buffer_size) override;
+  void reset() override;
+  void seek_to_sample(int sample) override;
+
+  int get_bits_per_sample() const override { return m_bits_per_sample; }
+  int get_rate() const override { return m_rate; }
+  int get_channels() const override { return m_channels; }
+  size_t get_size() const override { return m_size; }
+
+private:
+  int m_bits_per_sample;
+  int m_rate;
+  int m_channels;
+  size_t m_size;
+
+  size_t m_sample_pos;
+
+private:
+  ProceduralSoundFile(const ProceduralSoundFile&) = delete;
+  ProceduralSoundFile& operator=(const ProceduralSoundFile&) = delete;
+};
+
+#endif
+
+/* EOF */
