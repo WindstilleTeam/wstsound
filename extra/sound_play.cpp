82c99d86a1225054f3bc787ba06ea7d5ff545ca8
Ingo Ruhnke <grumbel@gmail.com>
2020-09-23 12:28:51 +0200

    Cleanup argument parsing in extra/sound_play

--- extra/sound_play.cpp
+++ extra/sound_play.cpp
@@ -128,10 +128,9 @@ int str2effect(std::string text) {
   }
 }
 
-} // namespace
-
 struct Options
 {
+  std::vector<std::string> files = {};
   bool loop = false;
   SoundSourceType source_type = SoundSourceType::STREAM;
   float seek = 0;
@@ -144,208 +143,198 @@ struct Options
   std::vector<std::optional<std::variant<float, int>>> dfltparam = {};
 };
 
-int main(int argc, char** argv)
+Options parse_args(int argc, char** argv)
 {
-  if (argc < 2)
-  {
+  if (argc < 2) {
     print_usage(argc, argv);
-    return 1;
+    exit(EXIT_SUCCESS);
   }
-  else
-  {
-    Options opts;
-    std::vector<std::string> files;
-    for (int i = 1; i < argc; ++i) {
-      if (argv[i][0] == '-') {
-        if (strcmp(argv[i], "--help") == 0) {
-          print_usage(argc, argv);
-          return 0;
-        } else if (strcmp(argv[i], "--loop") == 0) {
-          opts.loop = true;
-        } else if (strcmp(argv[i], "--stream") == 0) {
-          opts.source_type = SoundSourceType::STREAM;
-        } else if (strcmp(argv[i], "--static") == 0) {
-          opts.source_type = SoundSourceType::STATIC;
-        } else if (strcmp(argv[i], "--seek") == 0) {
-          if (++i >= argc) {
-            throw std::runtime_error("--seek needs an argument");
-          }
-          opts.seek = std::stof(argv[i]);
-        } else if (strcmp(argv[i], "--fadein") == 0) {
-          opts.fade = FadeState::FadingOn;
-        } else if (strcmp(argv[i], "--fadeout") == 0) {
-          opts.fade = FadeState::FadingOff;
-        } else if (strcmp(argv[i], "--effect") == 0) {
-          if (++i >= argc) {
-            throw std::runtime_error("--effect needs an argument");
-          }
-          opts.effect = str2effect(argv[i]);
-        } else if (strcmp(argv[i], "--fx-param") == 0) {
-          if (++i >= argc) {
-            throw std::runtime_error("--fx-param needs an argument");
-          }
 
-          auto const& values = string_split(argv[i], ':');
-          for(auto const& value : values) {
-            if (!value.empty()) {
-              if (value.back() == 'f') {
-                opts.fxparam.emplace_back(std::stof(value));
-              } else if (value.back() == 'i') {
-                opts.fxparam.emplace_back(std::stoi(value));
-              } else {
-                throw std::runtime_error("--fx-param must have a 'i' or 'f' suffix to indicate their type");
-              }
-            } else {
-              opts.fxparam.emplace_back();
-            }
-          }
-        } else if (strcmp(argv[i], "--filter") == 0) {
-          if (++i >= argc) {
-            throw std::runtime_error("--filter needs an argument");
-          }
-          opts.filter = str2filter(argv[i]);
-        } else if (strcmp(argv[i], "--flt-param") == 0) {
-          if (++i >= argc) {
-            throw std::runtime_error("--flt-param needs an argument");
-          }
+  Options opts;
 
-          auto const& values = string_split(argv[i], ':');
-          for(auto const& value : values) {
-            if (!value.empty()) {
-              if (value.back() == 'f') {
-                opts.fltparam.emplace_back(std::stof(value));
-              } else if (value.back() == 'i') {
-                opts.fltparam.emplace_back(std::stoi(value));
-              } else {
-                throw std::runtime_error("--flt-param must have a 'i' or 'f' suffix to indicate their type");
-              }
-            } else {
-              opts.fltparam.emplace_back();
-            }
-          }
-        } else if (strcmp(argv[i], "--direct-filter") == 0) {
-          if (++i >= argc) {
-            throw std::runtime_error("--direct-filter needs an argument");
-          }
-          opts.direct_filter = str2filter(argv[i]);
-        } else if (strcmp(argv[i], "--dflt-param") == 0) {
-          if (++i >= argc) {
-            throw std::runtime_error("--dflt-param needs an argument");
-          }
+  for (int i = 1; i < argc; ++i) {
+    auto next_arg = [&]{
+      if (++i >= argc) {
+        std::ostringstream msg;
+        msg << argv[i - 1] << " needs an argument";
+        throw std::runtime_error(msg.str());
+      }
+    };
 
-          auto const& values = string_split(argv[i], ':');
-          for(auto const& value : values) {
-            if (!value.empty()) {
-              if (value.back() == 'f') {
-                opts.dfltparam.emplace_back(std::stof(value));
-              } else if (value.back() == 'i') {
-                opts.dfltparam.emplace_back(std::stoi(value));
-              } else {
-                throw std::runtime_error("--dflt-param must have a 'i' or 'f' suffix to indicate their type");
-              }
-            } else {
-              opts.dfltparam.emplace_back();
-            }
+    auto arg_parse_list = [&](){
+      std::vector<std::optional<std::variant<float, int>>> result;
+      auto const& values = string_split(argv[i], ':');
+      for(auto const& value : values) {
+        if (!value.empty()) {
+          if (value.back() == 'f') {
+            result.emplace_back(std::stof(value));
+          } else if (value.back() == 'i') {
+            result.emplace_back(std::stoi(value));
+          } else {
+            std::ostringstream os;
+            os << argv[i - 1] << " must have a 'i' or 'f' suffix to indicate their type";
+            throw std::runtime_error(os.str());
           }
         } else {
-          std::ostringstream os;
-          os << "unknown option " << argv[i];
-          throw std::runtime_error(os.str());
+          result.emplace_back();
         }
+      }
+      return result;
+    };
+
+    if (argv[i][0] == '-') {
+      if (strcmp(argv[i], "--help") == 0) {
+        print_usage(argc, argv);
+        exit(EXIT_SUCCESS);
+      } else if (strcmp(argv[i], "--loop") == 0) {
+        opts.loop = true;
+      } else if (strcmp(argv[i], "--stream") == 0) {
+        opts.source_type = SoundSourceType::STREAM;
+      } else if (strcmp(argv[i], "--static") == 0) {
+        opts.source_type = SoundSourceType::STATIC;
+      } else if (strcmp(argv[i], "--seek") == 0) {
+        next_arg();
+        opts.seek = std::stof(argv[i]);
+      } else if (strcmp(argv[i], "--fadein") == 0) {
+        opts.fade = FadeState::FadingOn;
+      } else if (strcmp(argv[i], "--fadeout") == 0) {
+        opts.fade = FadeState::FadingOff;
+      } else if (strcmp(argv[i], "--effect") == 0) {
+        next_arg();
+        opts.effect = str2effect(argv[i]);
+      } else if (strcmp(argv[i], "--fx-param") == 0) {
+        next_arg();
+        opts.fxparam = arg_parse_list();
+      } else if (strcmp(argv[i], "--filter") == 0) {
+        next_arg();
+        opts.filter = str2filter(argv[i]);
+      } else if (strcmp(argv[i], "--flt-param") == 0) {
+        next_arg();
+        opts.fltparam = arg_parse_list();
+      } else if (strcmp(argv[i], "--direct-filter") == 0) {
+        next_arg();
+        opts.direct_filter = str2filter(argv[i]);
+      } else if (strcmp(argv[i], "--dflt-param") == 0) {
+        next_arg();
+        opts.dfltparam = arg_parse_list();
       } else {
-        files.emplace_back(argv[i]);
+        std::ostringstream os;
+        os << "unknown option " << argv[i];
+        throw std::runtime_error(os.str());
       }
+    } else {
+      opts.files.emplace_back(argv[i]);
+    }
+  }
+
+  return opts;
+}
+
+int run(int argc, char** argv)
+{
+  Options opts = parse_args(argc, argv);
+
+  SoundManager sound_manager;
+
+  sound_manager.set_gain(1.0f);
+  sound_manager.sound().set_gain(1.0f);
+  sound_manager.voice().set_gain(1.0f);
+
+  std::vector<SoundSourcePtr> sources;
+  for (auto const& filename : opts.files)
+  {
+    SoundSourcePtr source = sound_manager.sound().prepare(filename, opts.source_type);
+
+    source->set_looping(opts.loop);
+
+    if (opts.seek != 0) {
+      source->seek_to(opts.seek);
     }
 
-    SoundManager sound_manager;
-
-    sound_manager.set_gain(1.0f);
-    sound_manager.sound().set_gain(1.0f);
-    sound_manager.voice().set_gain(1.0f);
-
-    std::vector<SoundSourcePtr> sources;
-    for (auto const& filename : files)
-    {
-      SoundSourcePtr source = sound_manager.sound().prepare(filename, opts.source_type);
-
-      source->set_looping(opts.loop);
-
-      if (opts.seek != 0) {
-        source->seek_to(opts.seek);
-      }
-
-      if (opts.fade != FadeState::NoFading) {
-        source->set_fading(opts.fade, 5.0f);
-      }
-
-      if (opts.direct_filter != AL_FILTER_NULL) {
-        FilterPtr direct_filter = sound_manager.create_filter(opts.direct_filter);
-        for (size_t i = 0; i < opts.dfltparam.size(); ++i) {
-          if (opts.dfltparam[i]) {
-            if (std::holds_alternative<int>(*opts.dfltparam[i])) {
-              direct_filter->seti(static_cast<int>(i) + 1, std::get<int>(*opts.dfltparam[i]));
-            } else if (std::holds_alternative<float>(*opts.dfltparam[i])) {
-              direct_filter->setf(static_cast<int>(i) + 1, std::get<float>(*opts.dfltparam[i]));
-            }
-          }
-        }
-        source->set_direct_filter(direct_filter);
-      }
-
-      if (opts.effect != AL_EFFECT_NULL || opts.filter != AL_FILTER_NULL) {
-        auto slot = sound_manager.create_effect_slot();
-
-        if (opts.effect != AL_EFFECT_NULL) {
-          auto effect = sound_manager.create_effect(opts.effect);
-          for (size_t i = 0; i < opts.fxparam.size(); ++i) {
-            if (opts.fxparam[i]) {
-              if (std::holds_alternative<int>(*opts.fxparam[i])) {
-                effect->seti(static_cast<int>(i) + 1, std::get<int>(*opts.fxparam[i]));
-              } else if (std::holds_alternative<float>(*opts.fxparam[i])) {
-                effect->setf(static_cast<int>(i) + 1, std::get<float>(*opts.fxparam[i]));
-              }
-            }
-          }
-          slot->set_effect(effect);
-        }
-
-        FilterPtr filter;
-        if (opts.filter != AL_FILTER_NULL) {
-          filter = sound_manager.create_filter(opts.filter);
-          for (size_t i = 0; i < opts.fltparam.size(); ++i) {
-            if (opts.fltparam[i]) {
-              if (std::holds_alternative<int>(*opts.fltparam[i])) {
-                filter->seti(static_cast<int>(i) + 1, std::get<int>(*opts.fltparam[i]));
-              } else if (std::holds_alternative<float>(*opts.fltparam[i])) {
-                filter->setf(static_cast<int>(i) + 1, std::get<float>(*opts.fltparam[i]));
-              }
-            }
-          }
-        }
-
-        source->set_effect_slot(slot, filter);
-      }
-
-      sources.emplace_back(source);
+    if (opts.fade != FadeState::NoFading) {
+      source->set_fading(opts.fade, 5.0f);
     }
 
+    if (opts.direct_filter != AL_FILTER_NULL) {
+      FilterPtr direct_filter = sound_manager.create_filter(opts.direct_filter);
+      for (size_t i = 0; i < opts.dfltparam.size(); ++i) {
+        if (opts.dfltparam[i]) {
+          if (std::holds_alternative<int>(*opts.dfltparam[i])) {
+            direct_filter->seti(static_cast<int>(i) + 1, std::get<int>(*opts.dfltparam[i]));
+          } else if (std::holds_alternative<float>(*opts.dfltparam[i])) {
+            direct_filter->setf(static_cast<int>(i) + 1, std::get<float>(*opts.dfltparam[i]));
+          }
+        }
+      }
+      source->set_direct_filter(direct_filter);
+    }
+
+    if (opts.effect != AL_EFFECT_NULL || opts.filter != AL_FILTER_NULL) {
+      auto slot = sound_manager.create_effect_slot();
+
+      if (opts.effect != AL_EFFECT_NULL) {
+        auto effect = sound_manager.create_effect(opts.effect);
+        for (size_t i = 0; i < opts.fxparam.size(); ++i) {
+          if (opts.fxparam[i]) {
+            if (std::holds_alternative<int>(*opts.fxparam[i])) {
+              effect->seti(static_cast<int>(i) + 1, std::get<int>(*opts.fxparam[i]));
+            } else if (std::holds_alternative<float>(*opts.fxparam[i])) {
+              effect->setf(static_cast<int>(i) + 1, std::get<float>(*opts.fxparam[i]));
+            }
+          }
+        }
+        slot->set_effect(effect);
+      }
+
+      FilterPtr filter;
+      if (opts.filter != AL_FILTER_NULL) {
+        filter = sound_manager.create_filter(opts.filter);
+        for (size_t i = 0; i < opts.fltparam.size(); ++i) {
+          if (opts.fltparam[i]) {
+            if (std::holds_alternative<int>(*opts.fltparam[i])) {
+              filter->seti(static_cast<int>(i) + 1, std::get<int>(*opts.fltparam[i]));
+            } else if (std::holds_alternative<float>(*opts.fltparam[i])) {
+              filter->setf(static_cast<int>(i) + 1, std::get<float>(*opts.fltparam[i]));
+            }
+          }
+        }
+      }
+
+      source->set_effect_slot(slot, filter);
+    }
+
+    sources.emplace_back(source);
+  }
+
+  for (auto& source : sources) {
+    source->play();
+  }
+
+  while (!sources.empty()) {
     for (auto& source : sources) {
-      source->play();
+      std::cout << "pos: " << source->get_sample_pos() << " / " << source->get_sample_duration() << std::endl;
     }
 
-    while (!sources.empty()) {
-      for (auto& source : sources) {
-        std::cout << "pos: " << source->get_sample_pos() << " / " << source->get_sample_duration() << std::endl;
-      }
+    std::erase_if(sources, [](SoundSourcePtr const& source){
+      return !source->is_playing();
+    });
 
-      std::erase_if(sources, [](SoundSourcePtr const& source){
-        return !source->is_playing();
-      });
+    usleep(10000);
 
-      usleep(10000);
+    sound_manager.update(0.01f);
+  }
 
-      sound_manager.update(0.01f);
-    }
+  return 0;
+}
+
+} // namespace
+
+int main(int argc, char** argv) {
+  try {
+    return run(argc, argv);
+  } catch (std::exception const& err) {
+    std::cerr << "error: " << err.what() << std::endl;
+    return EXIT_FAILURE;
   }
 }
 
