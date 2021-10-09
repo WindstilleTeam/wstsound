a5dbd76a2d88f29f65e08a8691f61e2251997fd3
Ingo Ruhnke <grumbel@gmail.com>
2021-10-09 20:58:06 +0200

    Add extension list to extra/openal_info

--- extra/openal_info.cpp
+++ extra/openal_info.cpp
@@ -18,14 +18,41 @@
 
 #include <iostream>
 
+#include <al.h>
+
 #include <wstsound/openal_system.hpp>
+#include <wstsound/openal_real_device.hpp>
+#include <wstsound/effect_slot.hpp>
 
 using namespace wstsound;
 
 int main(int argc, char** argv)
 {
   OpenALSystem openal;
+  auto& device = openal.open_real_device();
+
   openal.print_openal_version(std::cout);
+
+  if (device.is_extension_present("ALC_EXT_EFX"))
+  {
+    std::cout << "EFX not supported" << std::endl;
+  }
+
+  { // check how many EffectsSlots there are
+    int max_effect_slots = 0;
+    try {
+      std::vector<EffectSlotPtr> slots;
+      for (int i = 0; i < 1000; ++i) {
+        slots.emplace_back(std::make_unique<EffectSlot>());
+        max_effect_slots += 1;
+      }
+    } catch (...) {}
+
+    std::cout << "Effects Slots: " << max_effect_slots << std::endl;
+  }
+
+  std::cout << "Max Auxiliary Sends: " << device.max_auxiliary_sends() << std::endl;
+
   return 0;
 }
 
